ef21d6c33f88dbb2fa23d8676195d95c551c2e49
Damian Montero <photoca@hotmail.com>
2019-02-12 21:12:38 -0500

    Quiz default ready

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -5,18 +5,18 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
-        quiz.Models.Quiz[] quizAnswers = new quiz.Models.Quiz[4];
-
-        public ChatHub () {
-            quizAnswers[0]  = new quiz.Models.Quiz("Argentina",new string[]{"buenos Aires","ArgentinaCity"},0);
-            quizAnswers[1]  = new quiz.Models.Quiz("Colombia",new string[]{"Bogota","Colombia City"},0);
-            quizAnswers[2]  = new quiz.Models.Quiz("United States",new string[]{"Washinton","USA City"},0);
-            quizAnswers[3]  = new quiz.Models.Quiz("Panama",new string[]{"Suaita","Panama City"},1);
-        }
+        quiz.Models.Quiz[] quizAnswers = new quiz.Models.Quiz[]{
+                        new quiz.Models.Quiz("Argentina",new string[]{"buenos Aires","ArgentinaCity"},0),
+                        new quiz.Models.Quiz("Colombia",new string[]{"Bogota","Colombia City"},0),
+                        new quiz.Models.Quiz("United States",new string[]{"Washinton","USA City"},0),
+                        new quiz.Models.Quiz("Panama",new string[]{"Suaita","Panama City"},1),
+                        };
 
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message + quizAnswers[0].Question);
+
+            await Clients.All.SendAsync("ReceiveMessage", user , message );
+
         }
     }
 
